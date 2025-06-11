f96a05e1461a9f5b95e2cb1e236f069ea46a84ca
lkhgit <lkh080108@gmail.com>
2025-06-11 14:37:23 +0900

    Update snn.py

--- snn.py
+++ snn.py
@@ -78,7 +78,7 @@ def run_single(output_name, mean=0.85, std=0.25, nprop=nprop_default, sprop=spro
   
   nn.clear()
   print("Retrieving images from the cue signal...")
-  nn.retrieve_from_cue(np.stack([relevant_noised_cue] * 5), mem_tags[:-1], omega, encode_images_no_tag(store_img_paths, [mean] * (n-1), [std] * (n-1)))
+  nn.retrieve_from_cue(np.stack([relevant_noised_cue] * 5), mem_tags[:-1], omega, encode_images_no_tag(store_img_paths, [mean] * (n-1), [std] * (n-1)), with_tag)
 
   if email_key is not None:
     send_email_with_zip(
